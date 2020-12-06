6551d20d4d170fb1568ce29df8af65764230e9ee
Mike Lynch <nycynik@gmail.com>
2020-12-06 10:32:46 -0800

    output: Added output folder

--- process.py
+++ process.py
@@ -15,7 +15,7 @@ cap = cv2.VideoCapture('./video/running.mov')
 
 if cap.isOpened() == False:
     raise ('Could not open video')
-    
+
 while True:
     ret, frame = cap.read()
 
@@ -35,14 +35,13 @@ while True:
         for i, id in enumerate(index):
             guess += f'{i+1}\t{id}\t{(output[0][id] * 100):.3}%\t{classes[id]}\n'
         print(guess)
-        # frame = cv2.puttext(frame, guess,
-        #                     (100, 100),
-        #                     cv2.FONT_HERSHEY_SIMPLEX,
-        #                     1, (255,255,255), 2)
-        
+
+        frame = cv2.putText(frame, guess,
+                            (100, 100),
+                            cv2.FONT_HERSHEY_SIMPLEX,
+                            1, (255, 255, 255), 2)
+
     else:
         break
 
 cap.release()
-
-        
